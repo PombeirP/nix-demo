852a3018268f18f8c68b21161d5426224ed3de9a
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-09-03 11:40:39 +0200

    Set merger

--- default.nix
+++ default.nix
@@ -1,18 +1,12 @@
 # Run `nix-instantiate --strict --json --eval default.nix | jq`
 let
-  aBoolean = true;
-  aPath = ./default.nix;
-  aNull = null;
-  anInteger = 123;
-  aFloatingPointNumber = 123.456;
-  aUrl = https://status.im;
-  aList = [ aBoolean aPath aNull ];
-  aSet = { # Sets are really the core of the language, since ultimately the Nix language is all about creating derivations,
-           # which are really just sets of attributes to be passed to build scripts.
-    aNull = aNull;
-    inherit aPath; # Same as aPath = aPath
-    aFunction = aFunction aList;
+  set1 = {
+    a = 1;
+    b = 2;
   };
-  aFunction = arg: builtins.head arg;
+  set2 = {
+    a = 3;
+    c = 1;
+  };
 
-in aSet
+in set1 // set2
