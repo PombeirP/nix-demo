d7f3cf3fe1e8af927f6314479487cd58954d4e29
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-09-03 11:36:43 +0200

    Types in Nix

--- default.nix
+++ default.nix
@@ -1,10 +1,18 @@
-# Run `nix-instantiate --strict --json --eval default.nix --arg greetIstanbul true -A message2 | jq`
-{ greetIstanbul }:
+# Run `nix-instantiate --strict --json --eval default.nix | jq`
+let
+  aBoolean = true;
+  aPath = ./default.nix;
+  aNull = null;
+  anInteger = 123;
+  aFloatingPointNumber = 123.456;
+  aUrl = https://status.im;
+  aList = [ aBoolean aPath aNull ];
+  aSet = { # Sets are really the core of the language, since ultimately the Nix language is all about creating derivations,
+           # which are really just sets of attributes to be passed to build scripts.
+    aNull = aNull;
+    inherit aPath; # Same as aPath = aPath
+    aFunction = aFunction aList;
+  };
+  aFunction = arg: builtins.head arg;
 
-rec {
-  message = "Hello ${if greetIstanbul then "Istanbul" else "everyone"}";
-  message2 = ''
-    The message is:
-    ${message}
-  '';
-}
+in aSet
