8f43c11d72bb594aed9047687b1b1d6fc13d30ed
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-09-03 10:56:07 +0200

    Attribute selectors

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
-# Run `nix-instantiate --strict --json --eval default.nix | jq`
-
+# Run `nix-instantiate --strict --json --eval default.nix -A message2 | jq`
 let
   greetee = "Istanbul";
 
 in {
   message = "Hello ${greetee}";
+  message2 = "Message 2";
 }
