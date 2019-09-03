3f4679c95c38623d977347bfa0f46a42eb50fa14
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2019-09-03 11:32:00 +0200

    Conditionals and inline expressions

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
-# Run `nix-instantiate --strict --json --eval default.nix --argstr greetee audience -A message2 | jq`
-{ greetee }:
+# Run `nix-instantiate --strict --json --eval default.nix --arg greetIstanbul true -A message2 | jq`
+{ greetIstanbul }:
 
 rec {
-  message = "Hello ${greetee}";
+  message = "Hello ${if greetIstanbul then "Istanbul" else "everyone"}";
   message2 = ''
     The message is:
     ${message}
